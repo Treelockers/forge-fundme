2db61273a944f1bf9bb46769fb33a426562fdb46
XmoDVO <xmo@derivio.xyz>
2023-08-28 19:15:47 +0800

    feat: 🎸 network config

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -4,5 +4,18 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 
 contract HelperConfig is Script {
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    function getSepoliadEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {}
+
     function run() external {}
 }
