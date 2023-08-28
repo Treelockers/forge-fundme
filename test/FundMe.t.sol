8999466271f125e31c7b5db3812ce1fb775c19ed
XmoDVO <xmo@derivio.xyz>
2023-08-28 18:03:05 +0800

    feat: 🎸 init

---
+++ test/FundMe.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundme;
+
+    function setUp() external {
+        fundme = new FundMe();
+    }
+
+    function testMINIMUM_USD() public {
+        assertEq(fundme.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(fundme.i_owner(), address(this));
+    }
+}
