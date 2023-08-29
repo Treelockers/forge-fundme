006836db69fa331ed80435f6626f92ba78027e10
XmoDVO <xmo@derivio.xyz>
2023-08-29 17:34:50 +0800

    feat: 🎸 script test

---
+++ test/integration/FundMe.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
+
+contract FundMeTestIntegration is Test {
+    FundMe fundme;
+    address USER = makeAddr("user");
+    uint constant SEND_VALUE = 0.1 ether;
+    uint constant STARTING_BALANCE = 10 ether;
+    uint constant GAS_PRICE = 1;
+
+    function setUp() external {
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundme = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testUserCanFundInteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        fundFundMe.fundFundMe(address(fundme));
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundme));
+
+        assertEq(address(fundme).balance, 0);
+    }
+}
